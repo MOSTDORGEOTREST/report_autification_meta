e799e2d6b4669d7ba7a6c3dec59081f5424dd522
Nick <tnick1502@mail.ru>
2024-06-04 16:08:03 +0300

    fix

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
 docker system prune -a -f
 docker rm $(docker ps -a -q) -f
 docker rmi $(docker images -a -q) -f
-cd /root/projects/report_autification_backend
+cd /root/projects/report_autification_backend/report_autification_backend
 git pull
-cd /root/projects/report_autification_front
+cd /root/projects/report_autification_front/report_autification_front
 git pull
 cd /root/projects/report_autification_meta
 git pull
